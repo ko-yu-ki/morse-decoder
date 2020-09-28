4be817d30e8c9a2d78eb019a53baa3e1bb03c317
ko-yu-ki <55734445+ko-yu-ki@users.noreply.github.com>
2020-09-28 23:59:54 +0300

    feat: morse decoder script

--- src/index.js
+++ src/index.js
@@ -37,9 +37,52 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const dotsDashes = {
+  '10': '.',
+  '11': '-',
+};
+
 function decode(expr) {
-    // write your solution here
+  let result = '';
+  let exprArray = new Array;
+  let dotsDashesArray = new Array;
+  let uno = -1;
+  let currentSubstr = '';
+  for(let i = 0; i < expr.length; i = i + 10) {
+    currentSubstr = expr.substring(i, i + 10); 
+    if (currentSubstr !== '**********') {
+      uno = currentSubstr.indexOf('1'); //начало значимых цифр
+      exprArray.push(currentSubstr.substring(uno));
+    }
+    else {
+      exprArray.push(' ');
+    }
+  }
+  for(let i = 0; i < exprArray.length; i++){
+    dotsDashesArray[i] = '';
+    if (exprArray[i] !== ' ') {
+      for(let j = 0; j < exprArray[i].length; j = j + 2){
+        if (exprArray[i].substring(j, j + 2) in dotsDashes) {
+        dotsDashesArray[i] = dotsDashesArray[i] + dotsDashes[exprArray[i].substring(j, j + 2)];
+        }
+      }
+    } else {
+     dotsDashesArray[i] = exprArray[i]; 
+    }
+  }
+//  console.log(exprArray);
+//  console.log(dotsDashesArray);
+  for(let i = 0; i < dotsDashesArray.length; i++){
+    if (dotsDashesArray[i] !== ' ') {
+      result += MORSE_TABLE[dotsDashesArray[i]]; 
+    } else {
+      result += ' ';
+    }
+  }
+//  console.log(result);
+  return result;
 }
+
 
 module.exports = {
     decode
